336c83897edcc745e5aea79732e847cfcdcecd08
sabbir <bsse1014@iit.du.ac.bd>
2021-07-27 19:17:13 +0600

    semi authentication done

--- src/app/Component/login/login.component.ts
+++ src/app/Component/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {AuthServiceService} from '../../services/auth-service/auth-service.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  formGroup: FormGroup;
+  constructor(private authService: AuthServiceService) { }
 
   ngOnInit(): void {
+    this.initForm();
   }
-
+  initForm(): void {
+    this.formGroup = new FormGroup({
+      username: new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required])
+    });
+  }
+  loginProcess(): void {
+    if (this.formGroup.valid){
+      this.authService.login(this.formGroup.value).subscribe(result => {
+        if (result.success){
+          console.log(result);
+          alert(result.message);
+        }else{
+          alert(result.message);
+        }
+      });
+    }
+  }
 }
